e7542c545999158871a8feedfafafe1c588e0a5c
Jamyle Elen <jamyleelen09@gmail.com>
2025-08-06 02:22:19 -0300

    feat: added CRUD and option to mark tasks as complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "tasks" #namespace
 
 urlpatterns = [
-    path('', views.tasks_home),
+    path('', views.tasks_home, name="home"),
     path('add/', views.tasks_add, name="add"),
+    path('remove/<int:id>', views.tasks_remove, name="remove"),
+    path('edit/<int:id>', views.tasks_edit, name="edit"),
+    path('mark_completed/<int:id>', views.tasks_mark_completed, name="mark_completed"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import TaskForm
+from django.http import HttpRequest
+from .models import TaskModel
 
 # Create your views here.
 # def tasks_home:
@@ -7,9 +9,43 @@ from django.http import HttpResponse
 
 def tasks_home(request):
     context = {
-        "name": "Jamyle"
+        "name": "Jamyle",
+        "tasks": TaskModel.objects.all()
     }
     return render(request, 'tasks/home.html', context)
 
 def tasks_add(request):
-    return HttpResponse("Adicionar tasks")
+    
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("tasks:home")
+    context = {
+        "form": TaskForm
+    }
+    return render(request, "tasks/add_tasks.html", context)
+
+def tasks_remove(request:HttpRequest, id):
+    task = get_object_or_404(TaskModel, id=id)
+    task.delete()
+    return redirect("tasks:home")
+
+def tasks_edit(request:HttpRequest, id):
+    task = get_object_or_404(TaskModel, id=id)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("tasks:home")
+    form = TaskForm(instance=task)
+    context = {
+        "form": form
+    }
+    return render(request, "tasks/edit_tasks.html", context)
+
+def tasks_mark_completed(request:HttpRequest, id):
+    task = get_object_or_404(TaskModel, id=id)
+    task.completed = True
+    task.save()
+    return redirect("tasks:home")
